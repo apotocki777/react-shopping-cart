ae361fa043976214c41b2826e589a9bbd7b04ad9
Alexander Potocki <apotocki@griddynamics.com>
2024-04-11 16:19:19 +0200

    Initial commit + first header part

---
+++ src/pages/header/Header.jsx
@@ -0,0 +1,9 @@
+import TopHeader from "./Top";
+
+export default function Header() {
+  return (
+    <header className="flec-col min-w-full flex-1">
+      <TopHeader></TopHeader>
+    </header>
+  );
+}
