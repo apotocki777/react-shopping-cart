4479bd42ec49a1d539c4b84afd0bb25ea1d59af3
Alexander Potocki <apotocki@griddynamics.com>
2024-04-25 16:54:00 +0200

    Added error page to the routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,11 +4,13 @@ import Home from "./pages/home/Home";
 import ProductPage from "./pages/products/Products";
 import ProductDetails from "./pages/product_details/Product-details";
 import Cart from "./pages/cart/Cart";
+import ErrorPage from "./pages/error_page/Error-page";
 
 const routes = [
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Home /> },
       { path: "products", element: <ProductPage></ProductPage> },
